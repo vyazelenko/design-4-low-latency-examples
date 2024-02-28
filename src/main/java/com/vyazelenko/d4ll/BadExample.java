eb317428d062a1db16286cfa91bc210362b9b135
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-02-28 23:49:28 +0100

    Rewrite example.

--- src/main/java/com/vyazelenko/d4ll/BadExample.java
+++ src/main/java/com/vyazelenko/d4ll/BadExample.java
@@ -1,7 +1,11 @@
 package com.vyazelenko.d4ll;
 
+import org.agrona.BitUtil;
+import org.agrona.collections.Hashing;
 import org.openjdk.jmh.annotations.*;
 
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 @Fork(3)
@@ -12,17 +16,39 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Benchmark)
 public class BadExample
 {
-    private long nextId = System.currentTimeMillis();
+    @Param({ "1048576" })
+    private int size;
+    private int next;
+    private long[] values;
+    private HashMap<Long, Integer> cache;
 
-    @Benchmark
-    public Long wrapper()
+    @Setup
+    public void setup()
     {
-        return ++nextId;
+        if (!BitUtil.isPowerOfTwo(size))
+        {
+            throw new IllegalArgumentException("Size must be a power of two, got: " + size);
+        }
+        values = new long[size];
+        cache = new HashMap<>(size);
+        for (int i = 0; i < size; i++)
+        {
+            values[i] = ThreadLocalRandom.current().nextLong(1_000_000_000_001L, Long.MAX_VALUE);
+            cache.put(values[i], Hashing.hash(values[i]));
+        }
     }
 
     @Benchmark
-    public long primitive()
+    public Integer cached()
     {
-        return ++nextId;
+        final int index = (next++ & (size - 1));
+        return cache.get(values[index]);
+    }
+
+    @Benchmark
+    public int calculated()
+    {
+        final int index = (next++ & (size - 1));
+        return Hashing.hash(values[index]);
     }
 }
