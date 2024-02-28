858ce87a669d110e234a3c7e1cc1cb617e9c619a
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-02-29 00:06:50 +0100

    Simplify.

--- src/main/java/com/vyazelenko/d4ll/BadExample.java
+++ src/main/java/com/vyazelenko/d4ll/BadExample.java
@@ -1,9 +1,10 @@
 package com.vyazelenko.d4ll;
 
-import org.agrona.BitUtil;
 import org.agrona.collections.Hashing;
 import org.openjdk.jmh.annotations.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
@@ -16,8 +17,7 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Benchmark)
 public class BadExample
 {
-    @Param({ "1048576" })
-    private int size;
+    private static final int SIZE = 1024 * 1024;
     private int next;
     private long[] values;
     private HashMap<Long, Integer> cache;
@@ -25,30 +25,32 @@ public class BadExample
     @Setup
     public void setup()
     {
-        if (!BitUtil.isPowerOfTwo(size))
-        {
-            throw new IllegalArgumentException("Size must be a power of two, got: " + size);
-        }
-        values = new long[size];
-        cache = new HashMap<>(size);
-        for (int i = 0; i < size; i++)
+        values = new long[SIZE];
+        cache = new HashMap<>(SIZE);
+        ArrayList<Long> list = new ArrayList<>(SIZE);
+        for (int i = 0; i < SIZE; i++)
         {
             values[i] = ThreadLocalRandom.current().nextLong(1_000_000_000_001L, Long.MAX_VALUE);
-            cache.put(values[i], Hashing.hash(values[i]));
+            list.add(values[i]);
+        }
+        Collections.shuffle(list);
+        for (final Long value : list)
+        {
+            cache.put(value, Hashing.hash(value));
         }
     }
 
     @Benchmark
     public Integer cached()
     {
-        final int index = (next++ & (size - 1));
+        final int index = (next++ & (SIZE - 1));
         return cache.get(values[index]);
     }
 
     @Benchmark
     public int calculated()
     {
-        final int index = (next++ & (size - 1));
+        final int index = (next++ & (SIZE - 1));
         return Hashing.hash(values[index]);
     }
 }
