cebcf1a3f0403a0e5b99c25cd1b30e6932c58b2e
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-02-28 21:27:02 +0100

    Remove excluded classes.

--- target/generated-sources/annotations/com/vyazelenko/d4ll/jmh_generated/BadExample_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.vyazelenko.d4ll.jmh_generated;
-public class BadExample_jmhType extends BadExample_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/com/vyazelenko/d4ll/jmh_generated/ListExample_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.vyazelenko.d4ll.jmh_generated;
-public class ListExample_jmhType extends ListExample_jmhType_B3 {
-}
-
